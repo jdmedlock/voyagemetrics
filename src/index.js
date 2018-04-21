b6d3c0ffa4ebe62714946ebc10168a36f322af38
jdmedlock <jdmedlock@gmail.com>
2018-04-21 16:20:46 -0500

    feature: Filter out Voyage org admins
    Filter out rows for Voyage org admins.
    
    Resolves: Issue #6
    See also: N/a

--- src/index.js
+++ src/index.js
@@ -1,6 +1,48 @@
-import { ACTIVITY_PASSIVE, ACTIVITY_MANAGING, ACTIVIT_DEVELOPING,
+import { ACTIVITY_PASSIVE, ACTIVITY_MANAGING, ACTIVITY_DEVELOPING,
   ACTIVITY_PUBLISHING, ghEventMetrics } from './ghEventMetrics';
+import voyageAdmins from '../voyageAdmins.json';
 import eventJSON from '/Users/jim.medlock/Downloads/voyage4_events_20180419.json';
+
+/**
+ * @description Find a matching entry in the Voyage admin array matching
+ * the input GitHub account name..
+ * @param {String} actor GitHub account name
+ * @returns {Number} index of the matching event
+ */
+function findAdminByActor(actor) {
+  "use strict";
+  return voyageAdmins["gh-admin-accounts"].findIndex((element) => {
+    return element === actor;
+  });
+}
+
+/**
+ * @description Find a matching event in the GitHub Event Metric array
+ * which matches the input event.
+ * @param {String} eventName Input event name
+ * @returns {Number} index of the matching event
+ */
+function findQualifyingEvent(eventName) {
+  "use strict";
+  return ghEventMetrics.findIndex((element) => {
+    return element.event === eventName && element.deprecated === false &&
+      element.weight !== ACTIVITY_PASSIVE;
+  });
+}
+
+/**
+ * @description Find a matching entry in the aggregate results array matching
+ * the input team name and GitHub account name of a team member.
+ * @param {String} teamName Voyage team name
+ * @param {String} actor Team member account name
+ * @returns {Number} index of the matching event
+ */
+function findResultByActor(teamName, actor) {
+  "use strict";
+  return aggregateResults.findIndex((element) => {
+    return element.team === teamName && element.name === actor;
+  });
+}
 
 const NOT_FOUND = -1;
 
@@ -13,34 +55,7 @@ const NOT_FOUND = -1;
 // identifying if its for a team or team member, and the remaining
 // columns contains the accumulated count for a specific metric.
 let aggregateResults = [];
-
-let memberMetrics = ghEventMetrics.map((element) => { return 0; });
-/**
- * @description Find a matching event in the GitHub Event Metric array
- * which matches the input event.
- * @param {String} event Input event name
- * @returns {Number} index of the matching event
- */
-function findQualifyingEvent(event) {
-  "use strict";
-  return ghEventMetrics.findIndex((element, index) => {
-    return element.event === event.type && element.deprecated === false &&
-      element.weight !== ACTIVITY_PASSIVE;
-  });
-}
-/**
- * @description Find a matching entry in the aggregate results array matching
- * the input team name and GitHub account name of a team member.
- * @param {String} teamName Voyage team name
- * @param {String} event Team member account name
- * @returns {Number} index of the matching event
- */
-function findResultByActor(teamName, event) {
-  "use strict";
-  return aggregateResults.findIndex((element, index) => {
-    return element.team === teamName && element.name === event.actor;
-  });
-}
+let memberMetrics = [];
 
 (function() {
   "use strict";
@@ -54,25 +69,30 @@ function findResultByActor(teamName, event) {
     // Process each team event and accumulate it in both the team and
     // member counts
     for (const key in team.repo.events) {
-      const event = team.repo.events[key];
-      const eventIndex = findQualifyingEvent(event);
+      let eventActor = team.repo.events[key].actor;
+      const eventName = team.repo.events[key].type;
+      const eventIndex = findQualifyingEvent(eventName);
+
+      // Determine if the actor is one of the Organization administrators
+      const isAdmin = findAdminByActor(eventActor) === NOT_FOUND;
+
       if (eventIndex !== NOT_FOUND) {
-
         // Search the aggregate results array to increment the count for the
-        // current team, user, and event. Push the user onto the array if they haven't
-        // been added yet.
-        const memberIndex = findResultByActor(teamName, event);
-
-        if (memberIndex === NOT_FOUND) {
-          memberMetrics = ghEventMetrics.map((element) => { return 0; });
-          memberMetrics[eventIndex] += ghEventMetrics[eventIndex].weight;
-          aggregateResults.push({
-            team: teamName,
-            name: event.actor,
-            metrics: memberMetrics,
-          });
-        } else {
-          aggregateResults[memberIndex].metrics[eventIndex] += ghEventMetrics[eventIndex].weight;
+        // current team, user, and event. Push the user onto the array if they
+        // haven't been added yet.
+        const memberIndex = findResultByActor(teamName, eventActor);
+        if (isAdmin) {
+          if (memberIndex === NOT_FOUND) {
+            memberMetrics = ghEventMetrics.map((element) => { return 0; });
+            memberMetrics[eventIndex] += ghEventMetrics[eventIndex].weight;
+            aggregateResults.push({
+              team: teamName,
+              name: eventActor,
+              metrics: memberMetrics,
+            });
+          } else {
+            aggregateResults[memberIndex].metrics[eventIndex] += ghEventMetrics[eventIndex].weight;
+          }
         }
       }
     }
@@ -94,6 +114,6 @@ function findResultByActor(teamName, event) {
       return outputValues;
       }, '');
     console.log(element.team.split('-')[0] + ', ' + element.team + ', ' +
-      element.name + ' ' + metricValues);
+      element.name + metricValues);
   });
 }());
